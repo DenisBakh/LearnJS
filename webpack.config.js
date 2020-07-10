493d7f8054103689ab61c76cbf8c564412785468
DenBakh <den.bah89@gmail.com>
2020-07-10 20:59:55 +0700

    Added context path 'src'

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+	context: path.resolve(__dirname, 'src'), // исходники по умолчанию
 	mode: 'development',
 	entry: {
-		main: './src/index.js',
+		main: './index.js',
 	},
 	output: {
-		filename: '[name].js',
+		filename: '[name].[hash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './src/index.html'
+			template: './index.html'
 		}),
 		new CleanWebpackPlugin()
 	]
